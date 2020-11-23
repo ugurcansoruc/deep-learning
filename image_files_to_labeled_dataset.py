93d10dc1d0b5389d8b25870714418a56b897b270
Ugurcan <ugurcan.soruc@gmail.com>
2020-11-24 01:15:46 +0300

    Update image_files_to_labeled_dataset.py

--- image_files_to_labeled_dataset.py
+++ image_files_to_labeled_dataset.py
@@ -5,6 +5,15 @@ print(tf.__version__)
 
 
 def function():
+    """
+    main_directory/
+    ...class_a/
+    ......a_image_1.jpg
+    ......a_image_2.jpg
+    ...class_b/
+    ......b_image_1.jpg
+    ......b_image_2.jpg
+    """
     dataset = tf.keras.preprocessing.image_dataset_from_directory(
         'C:/Users/ugurc/Desktop/main_directory',
         batch_size=64,
